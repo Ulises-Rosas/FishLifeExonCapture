46378ac21e75b69670a397cbd6212188e9c027e9
Ulises-Rosas <ulisesfrosasp@gmail.com>
2020-04-15 14:19:43 -0500

    introducing exon datasets

--- setup.py
+++ setup.py
@@ -13,8 +13,14 @@ fishlifefiles = [
                 # "ProbeSets/*",
                 "map-exons-list.txt",
                 "map-exons-othophysi-list.txt",
-                "all_Master.fasta*",          # heavy install
-                "ALL_Master_Otophysi.fasta*"  # heavy install
+                # "all_Master.fasta*",          # heavy install
+                # "ALL_Master_Otophysi.fasta*"  # heavy install
+                "ExonList.txt",
+                "MitochondrialExonList.txt",
+                "ReadingFramesPercomorph/*",
+                "ReadingFramesElopomorph/*",
+                "ReadingFramesOtophysi/*",
+                "ReadingFramesOsteoglossomorph/*"
                 ]
     
 setup(
@@ -28,12 +34,13 @@ setup(
                     "fishlifedat"          : fishlifefiles },
     entry_points = {
         'console_scripts': [
-            'fishmanager         = fishlifeexoncapture.fishmanager:main',
-            'trimmomatic-loop-PE = fishlifeexoncapture.step1:main',
-            'map-exons           = fishlifeexoncapture.step2a:main',
-            'map-exons-otophysi  = fishlifeexoncapture.step2b:main',
-            'initialVelvet       = fishlifeexoncapture.step3:main',
-            'runaTRAM            = fishlifeexoncapture.step4:main'
+            'fishmanager             = fishlifeexoncapture.fishmanager:main',
+            'trimmomatic-loop-PE     = fishlifeexoncapture.step1:main',
+            'map-exons               = fishlifeexoncapture.step2a:main',
+            'map-exons-otophysi      = fishlifeexoncapture.step2b:main',
+            'initialVelvet           = fishlifeexoncapture.step3:main',
+            'runaTRAM                = fishlifeexoncapture.step4:main',
+            'ExonFilteringPercomorph = fishlifeexoncapture.step5percomorph:main'
             ]
     },
     install_requires = dependencies,
