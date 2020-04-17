4ad7503657d3b436db4325a973e0b551335765de
Ulises-Rosas <ulisesfrosasp@gmail.com>
2020-04-17 17:23:53 -0500

    update with getexon utility

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
+import pickle
 import subprocess
+import fishlifedat
 
 def runShell(args, type = ""):
 
@@ -52,7 +54,67 @@ def getdict(filename):
         out[ tmpl[0] ] = tmpl[1:]
     return out
 
-def check_reqs(requirements, labels):
+def check_reqs(requirements, labels, allvals = False):
     # requirements = ["a", "b"]
     # labels = {"a":1, "b":3}
-    return any( [True for i in requirements if i in labels.keys()] )
+    tmp_bools = [True for i in requirements if i in labels.keys()] 
+
+    return all(tmp_bools) if allvals else any(tmp_bools)
+
+def fas_to_dic(file):
+    
+    file_content = open(file, 'r').readlines()
+    seqs_list   = []
+    
+    for i in file_content:
+        seqs_list.append(i.replace("\n", "")) 
+    
+    keys = [] 
+    values = []    
+    i = 0
+    while(">" in seqs_list[i]):
+        keys.append(seqs_list[i])
+        i += 1 
+        JustOneValue = []
+
+        while((">" in seqs_list[i]) == False):
+            JustOneValue.append(seqs_list[i]) 
+            i += 1
+
+            if(i == len(seqs_list)):
+                i -= 1
+                break
+
+        values.append("".join(JustOneValue).upper())
+        
+    return dict(zip(keys, values))
+
+def getexons(tmp_dir):
+    
+    get_se  = lambda p,f,s: [i.strip() + s for i in open(os.path.join(p,f), "r")]
+    interse = lambda el,se: set(el.keys()) & set(se)
+    lookfor = lambda s,d,t: {(t,i):d[i] for i in s}
+
+    main_path = fishlifedat.__path__[0]
+    exonlists = [
+        ('nucl',"ExonList.txt"), 
+        ('mito', "MitochondrialExonList.txt")
+        ]
+
+    tmp_path  = os.path.join(main_path, tmp_dir + ".dict")
+
+    with open(tmp_path, "rb") as f:
+        tmp_dict = pickle.loads(f.read())
+
+    exons = tmp_dict[tmp_dir]
+    out   = {}
+    for tup in exonlists:
+
+        label, file = tup
+        tmp_exons   = get_se(main_path, file, ".fasta")
+        tmp_inter   = interse(exons, tmp_exons)
+
+        out.update( lookfor(tmp_inter, exons, label) )
+
+    return out
+
